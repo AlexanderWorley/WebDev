8a8724f935cb8adcb330c61f37164091b6b84bd5
Alexander Worley <jAlexanderworley@gmail.com>
2020-06-15 13:10:54 -0400

    Updated connection for testing

--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -14,7 +14,7 @@ namespace DataLibrary.DataAccess
 {
     public static class SqlDataAccess
     {
-        public static string GetConnectionString(string connectionName = "inventoryserverccfi/InventoryDB") 
+        public static string GetConnectionString(string connectionName = "inventoryservercffi/InventoryDB") 
         {
             return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
         }
