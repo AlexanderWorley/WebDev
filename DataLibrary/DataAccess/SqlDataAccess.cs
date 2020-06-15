056de03931a78b85a5936139265ceb4cf7c38e79
Alexander Worley <jAlexanderworley@gmail.com>
2020-06-15 12:49:58 -0400

    Update SqlDataAccess.cs

--- DataLibrary/DataAccess/SqlDataAccess.cs
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -14,7 +14,7 @@ namespace DataLibrary.DataAccess
 {
     public static class SqlDataAccess
     {
-        public static string GetConnectionString(string connectionName = "Inventory") 
+        public static string GetConnectionString(string connectionName = "InventoryDB") 
         {
             return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
         }
